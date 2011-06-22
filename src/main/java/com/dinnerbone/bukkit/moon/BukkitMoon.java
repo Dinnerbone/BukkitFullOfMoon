5461782af0f9b545df51c96024126166bf929641
Dinnerbone <dinnerbone@dinnerbone.com>
2011-06-22 18:48:41 +0100

    You may now use the moon generator as your default world

--- src/main/java/com/dinnerbone/bukkit/moon/BukkitMoon.java
+++ src/main/java/com/dinnerbone/bukkit/moon/BukkitMoon.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -38,5 +39,10 @@ public class BukkitMoon extends JavaPlugin {
         }
 
         return moon;
+    }
+
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        return new MoonChunkGenerator();
     }
 }
