958bb5776c0f6c3ee66478378dbd0a265bcca328
Dinnerbone <dinnerbone@dinnerbone.com>
2011-06-26 17:39:35 +0100

    Vary the terrain height a little

--- src/main/java/com/dinnerbone/bukkit/moon/MoonChunkGenerator.java
+++ src/main/java/com/dinnerbone/bukkit/moon/MoonChunkGenerator.java
@@ -9,14 +9,35 @@ import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.util.noise.NoiseGenerator;
+import org.bukkit.util.noise.SimplexNoiseGenerator;
 
 public class MoonChunkGenerator extends ChunkGenerator {
+    private NoiseGenerator generator;
+
+    private NoiseGenerator getGenerator(World world) {
+        if (generator == null) {
+            generator = new SimplexNoiseGenerator(world);
+        }
+
+        return generator;
+    }
+
+    private int getHeight(World world, double x, double y, int variance) {
+        NoiseGenerator gen = getGenerator(world);
+
+        double result = gen.noise(x, y);
+        result *= variance;
+        return NoiseGenerator.floor(result);
+    }
+    
     public byte[] generate(World world, Random random, int cx, int cz) {
         byte[] result = new byte[32768];
 
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                for (int y = 0; y < 50; y++) {
+                int height = getHeight(world, cx + x * 0.0625, cz + z * 0.0625, 2) + 60;
+                for (int y = 0; y < height; y++) {
                     result[(x * 16 + z) * 128 + y] = (byte)Material.SPONGE.getId();
                 }
             }
@@ -25,10 +46,7 @@ public class MoonChunkGenerator extends ChunkGenerator {
         return result;
     }
 
-    public boolean canSpawn(World world, int x, int z) {
-        return true;
-    }
-
+    @Override
     public List<BlockPopulator> getDefaultPopulators(World world) {
         return Arrays.asList((BlockPopulator)new MoonCraterPopulator(), new FlagPopulator());
     }
