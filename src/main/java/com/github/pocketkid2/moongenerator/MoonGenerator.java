2ffe90bb4ee4e40dde378134c7b19eb126bee516
Adam Taylor <afranklint16@gmail.com>
2021-07-24 16:32:36 -0600

    Strip out unnecessary command

--- src/main/java/com/github/pocketkid2/moongenerator/MoonGenerator.java
+++ src/main/java/com/github/pocketkid2/moongenerator/MoonGenerator.java
@@ -1,34 +1,21 @@
 package com.github.pocketkid2.moongenerator;
 
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.pocketkid2.moongenerator.chunkgenerators.MoonChunkGenerator;
 
 public class MoonGenerator extends JavaPlugin {
-	private final static String WORLD_NAME = "world_moon";
-	private static World moon = null;
 
 	@Override
 	public void onEnable() {
-		// Create command executor
-		getCommand("moon").setExecutor(new MoonCommandExec());
-		// Log status
-		this.getLogger().info("Enabled!");
+		getLogger().info("Enabled!");
 	}
 
-	// Creates and returns a world
-	public static World getMoon() {
-		if (moon == null) {
-			WorldCreator creator = new WorldCreator(WORLD_NAME);
-			creator.environment(World.Environment.NORMAL);
-			creator.generator(new MoonChunkGenerator());
-			moon = Bukkit.getServer().createWorld(creator);
-		}
-		return moon;
+	@Override
+	public void onDisable() {
+		getLogger().info("Disabled!");
+
 	}
 
 	@Override
