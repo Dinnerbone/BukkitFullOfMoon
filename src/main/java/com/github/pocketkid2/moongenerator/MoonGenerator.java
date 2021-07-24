6dc59d42c4b35ccb87226808728849c0479c6cca
Adam Taylor <afranklint16@gmail.com>
2021-07-24 17:42:05 -0600

    Fixed terrain and implemented configuration

--- src/main/java/com/github/pocketkid2/moongenerator/MoonGenerator.java
+++ src/main/java/com/github/pocketkid2/moongenerator/MoonGenerator.java
@@ -7,8 +7,22 @@ import com.github.pocketkid2.moongenerator.chunkgenerators.MoonChunkGenerator;
 
 public class MoonGenerator extends JavaPlugin {
 
+	private int terrain_height_variance = 5;
+	private double terrain_noise_scale = 0.015625;
+	private int terrain_depth = 60;
+
+	private boolean craters;
+	private boolean flags;
+
 	@Override
 	public void onEnable() {
+		saveDefaultConfig();
+		var cfg = getConfig();
+		terrain_height_variance = cfg.getInt("height-variance", 5);
+		terrain_noise_scale = 1.0D / cfg.getInt("noise-scale", 64);
+		terrain_depth = cfg.getInt("depth", 60);
+		craters = cfg.getBoolean("craters", true);
+		flags = cfg.getBoolean("flags", true);
 		getLogger().info("Enabled!");
 	}
 
@@ -20,6 +34,26 @@ public class MoonGenerator extends JavaPlugin {
 
 	@Override
 	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
-		return new MoonChunkGenerator();
+		return new MoonChunkGenerator(this);
+	}
+
+	public int getHeightVariance() {
+		return terrain_height_variance;
+	}
+
+	public double getNoiseScale() {
+		return terrain_noise_scale;
+	}
+
+	public int getTerrainDepth() {
+		return terrain_depth;
+	}
+
+	public boolean areCratersOn() {
+		return craters;
+	}
+
+	public boolean areFlagsOn() {
+		return flags;
 	}
 }
--- src/main/java/com/github/pocketkid2/moongenerator/chunkgenerators/MoonChunkGenerator.java
+++ src/main/java/com/github/pocketkid2/moongenerator/chunkgenerators/MoonChunkGenerator.java
@@ -1,6 +1,6 @@
 package com.github.pocketkid2.moongenerator.chunkgenerators;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,59 +12,68 @@ import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.util.noise.NoiseGenerator;
 import org.bukkit.util.noise.SimplexNoiseGenerator;
 
+import com.github.pocketkid2.moongenerator.MoonGenerator;
+import com.github.pocketkid2.moongenerator.blockpopulators.CraterPopulator;
 import com.github.pocketkid2.moongenerator.blockpopulators.FlagPopulator;
-import com.github.pocketkid2.moongenerator.blockpopulators.MoonCraterPopulator;
 
 public class MoonChunkGenerator extends ChunkGenerator {
+
+	private static final int CHUNK_SIZE = 16;
+
 	private NoiseGenerator generator;
 
+	private MoonGenerator plugin;
+
+	public MoonChunkGenerator(MoonGenerator p) {
+		plugin = p;
+	}
+
 	private NoiseGenerator getGenerator(World world) {
-		if (generator == null) {
+		if (generator == null)
 			generator = new SimplexNoiseGenerator(world);
-		}
 		return generator;
 	}
 
-	private int getHeight(World world, double x, double y, int variance) {
-		NoiseGenerator gen = getGenerator(world);
-		double result = gen.noise(x, y);
-		result *= variance;
-		return NoiseGenerator.floor(result);
+	private int getHeight(World world, double x, double y) {
+		var gen = getGenerator(world);
+		var result = plugin.getHeightVariance() * gen.noise(x * plugin.getNoiseScale(), y * plugin.getNoiseScale());
+		return plugin.getTerrainDepth() + NoiseGenerator.floor(result);
 	}
 
 	@Override
 	public ChunkGenerator.ChunkData generateChunkData(World world, Random random, int cx, int cz, ChunkGenerator.BiomeGrid biome) {
-		// Create the chunk data from method
-		ChunkGenerator.ChunkData data = this.createChunkData(world);
+		var data = createChunkData(world);
 		// Loop through each X and Z value
-		for (int x = 0; x < 16; x++) {
-			for (int z = 0; z < 16; z++) {
-				int height = this.getHeight(world, cx + (x * 0.0625), cz + (z * 0.625), 2) + 60;
-				// Loop through each Y value in the column
-				for (int y = 0; y < height; y++) {
-					// Set the block to be stone if more than 5 blocks down from the surface
-					if (y < (height - 1)) {
-						data.setBlock(x, y, z, Material.STONE);
-					} else {
+		for (var x = 0; x < CHUNK_SIZE; x++)
+			for (var z = 0; z < CHUNK_SIZE; z++) {
+				var height = getHeight(world, cx * CHUNK_SIZE + x, cz * CHUNK_SIZE + z);
+				for (var y = 0; y < height; y++)
+					if (y == 0)
+						data.setBlock(x, y, z, Material.BEDROCK);
+					else if (y == height - 1)
 						data.setBlock(x, y, z, Material.GRAVEL);
-					}
-				}
+					else
+						data.setBlock(x, y, z, Material.STONE);
 			}
-		}
 		return data;
 	}
 
 	@Override
 	public List<BlockPopulator> getDefaultPopulators(World world) {
-		return Arrays.asList((BlockPopulator)new MoonCraterPopulator(), new FlagPopulator());
+		List<BlockPopulator> populators = new ArrayList<>();
+		if (plugin.areCratersOn())
+			populators.add(new CraterPopulator());
+		if (plugin.areFlagsOn())
+			populators.add(new FlagPopulator());
+		return populators;
 	}
 
 	@Override
 	public Location getFixedSpawnLocation(World world, Random random) {
 		// Create random position within 100 blocks of 0,0
-		int x = random.nextInt(200) - 100;
-		int z = random.nextInt(200) - 100;
-		int y = world.getHighestBlockYAt(x, z);
+		var x = random.nextInt(200) - 100;
+		var z = random.nextInt(200) - 100;
+		var y = world.getHighestBlockYAt(x, z);
 		return new Location(world, x, y, z);
 	}
 }
